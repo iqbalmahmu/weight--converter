62d42f8589363227eeaa573771b1603a5064ffd6
foysal <mdrad43@gmail.com>
2019-05-18 00:03:58 +0800

    added some html markup on weight-converter app

---
+++ app.js
@@ -0,0 +1,17 @@
+// var name = 'foysal';
+
+// var count = name.length;
+
+// console.log(count);
+// 
+// let name = 'foysal';
+
+// let count = name.length;
+
+// console.log(count);
+
+const name = 'foysal';
+
+const count = name.length;
+
+console.log(count);
